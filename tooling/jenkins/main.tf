a7492fa8351bc923161d84c9ca8ef9cefce77042
Nikolai Yatsyna <nikolai.yatsyna@gmail.com>
2020-05-28 22:45:44 +0300

    jenkins as a module
    Signed-off-by: Nikolai Yatsyna <nikolai.yatsyna@gmail.com>

---
+++ tooling/jenkins/main.tf
@@ -0,0 +1,119 @@
+provider "kubernetes" {
+    load_config_file = false
+    host = var.gke_host
+    cluster_ca_certificate = base64decode(var.gke_cluster_cert)
+    username = var.master_username
+    password = var.master_password
+}
+
+resource "kubernetes_namespace" "jenkins" {
+    metadata {
+        name = "jenkins"
+        labels = {
+            "app.kubernetes.io/managed-by" = "terraform"
+        }
+    }
+}
+
+resource "kubernetes_deployment" "jenkins-deployment" {
+    metadata {
+        name = "jenkins-master-deployment"
+        namespace = kubernetes_namespace.jenkins.metadata.0.name
+        labels = {
+            "service" = "jenkins-master-service"
+            "app.kubernetes.io/managed-by" = "terraform"
+        }
+    }
+    spec {
+        replicas = 1
+        strategy {
+            type = "Recreate"
+        }
+        selector {
+            match_labels = {
+                service = "jenkins-master-service"
+            }
+        }
+        template {
+            metadata {
+                name = "jenkins-master"
+                namespace = kubernetes_namespace.jenkins.metadata.0.name
+                labels = {
+                    "service" = "jenkins-master-service"
+                    "app.kubernetes.io/managed-by" = "terraform"
+                }
+            }
+            spec {
+                container {
+                    image = "${data.google_container_registry_repository.registry.repository_url}/${var.jenkins_docker_image}:${var.jenkins_docker_image_version}"
+                    name = "jenkins-master"
+                    env {
+                        name = "JENKINS_OPTS"
+                        value = "--prefix=/jenkins"
+                    }
+                    volume_mount {
+                        name = "jenkins-volume"
+                        sub_path = "jenkins"
+                        mount_path = "/var/jenkins_home"
+                    }
+                    readiness_probe {
+                        initial_delay_seconds = 60
+                        period_seconds = 15
+                        failure_threshold = 5
+                        http_get {
+                            scheme = "HTTP"
+                            path = "/jenkins/login"
+                            port = "8080"
+                        }
+                    }
+                    security_context {
+                        // GKE mounts volume as root. Container started as non-root can't access volume
+                        privileged = true
+                        allow_privilege_escalation = true
+                    }
+                    port {
+                        name = "http-port"
+                        container_port = 8080
+                    }
+                    port {
+                        name = "jnlp-port"
+                        container_port = 50000
+                    }
+                }
+                volume {
+                    name = "jenkins-volume"
+                    gce_persistent_disk {
+                        pd_name = var.pd_name
+                    }
+                }
+            }
+        }
+    }
+}
+
+resource "kubernetes_service" "jenkins-master" {
+    metadata {
+        name = "jenkins-master-service"
+        namespace = kubernetes_namespace.jenkins.metadata.0.name
+        labels = {
+            "app.kubernetes.io/managed-by" = "terraform"
+        }
+    }
+    spec {
+        type = "LoadBalancer"
+        selector = {
+            service = kubernetes_deployment.jenkins-deployment.metadata[0].labels.service
+        }
+        session_affinity = "ClientIP"
+        port {
+            name = "https"
+            port = 80
+            target_port = 8080
+        }
+        port {
+            name = "jnlp-port"
+            port = 50000
+            target_port = 50000
+        }
+    }
+}
---
+++ tooling/jenkins/variables.tf
@@ -0,0 +1,4 @@
+variable "master_username" {}
+variable "master_password" {}
+variable "gke_host" {}
+variable "gke_cluster_cert" {}
